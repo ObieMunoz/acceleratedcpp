dcd5e599738e0e8e77e0f572d746e7d3ae8b2158
Obie <obiemunozjr@gmail.com>
2022-06-24 23:18:27 -0400

    Completed chapter 0

---
+++ chapter 0/0-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "This is an experiment and\t we just did a tab." << std::endl;
+    std::cout << "|\t| One Tab" << std::endl;
+    std::cout << "|\t\t| Two Tabs" << std::endl;
+    std::cout << "|\t\t\t| Three Tabs" << std::endl;
+    std::cout << "|\t\t\t\t| Four Tabs" << std::endl;
+    return 0;
+}
